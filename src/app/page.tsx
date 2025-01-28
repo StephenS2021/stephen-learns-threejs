104fa233441a1c9f01bf93b400d29f3f8ee8d9c4
StephenS2021 <sspencerwong28@gmail.com>
2025-01-28 17:49:53 -0500

    fixing nextjs client component errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
-
+// import dynamic from "next/dynamic";
 import P5Wrapper from "@/components/P5Wrapper";
 // import BezierSketch from "@/sketches/BezierSketch";
 import stripTerrainSketch from "@/sketches/stripTerrainSketch";
+
+
 export default function Home() {
   return (
     <div>
-      <P5Wrapper sketch={stripTerrainSketch} />
+      <P5Wrapper sketch={stripTerrainSketch}/>
       <p className="h-screen flex items-center justify-center text-[#4CBB17] text-[200px]">oenis</p>
     </div>
   );
