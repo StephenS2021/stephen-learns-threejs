fbc30c35f3e08238693f975c7605cb0c09c58622
StephenS2021 <sspencerwong28@gmail.com>
2025-01-28 16:58:22 -0500

    Initial commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Head } from "next/document";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,6 +25,29 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link
+          rel="preload"
+          href="/fonts/murberry-font/Murberry-rgxnp.woff2"
+          as="font"
+          type="font/woff2"
+          crossOrigin="anonymous"
+        />
+        <link
+          rel="preload"
+          href="/fonts/murberry-font/Murberry-rgxnp.ttf"
+          as="font"
+          type="font/ttf"
+          crossOrigin="anonymous"
+        />
+        <link
+          rel="preload"
+          href="/fonts/murberry-font/Murberry-YzE2a.otf"
+          as="font"
+          type="font/otf"
+          crossOrigin="anonymous"
+        />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
---
+++ src/types/p5-global.d.ts
@@ -0,0 +1,7 @@
+declare module 'p5';
+
+import * as p5 from "p5";
+
+declare module "p5" {
+    export const VIDEO: string;
+}
