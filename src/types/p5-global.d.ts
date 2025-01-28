088a3c2e8ebcc75bb500a4ae49301ca129427f02
StephenS2021 <sspencerwong28@gmail.com>
2025-01-28 17:15:25 -0500

    Eslint still erroring

--- src/types/p5-global.d.ts
+++ src/types/p5-global.d.ts
@@ -1,1 +1,7 @@
+/* eslint-disable-next-line @typescript-eslint/no-unused-vars */
 import p5 from "p5";
+
+declare global {
+    const p5Instance: p5; // Explicit use of p5 type
+    const p5: typeof import("p5");
+  }
